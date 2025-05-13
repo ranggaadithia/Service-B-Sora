8b16e0cfd6160b484cc54c65b9c6e87f5db12359
Rangga Adithia <rangga.adithia26@gmail.com>
2025-05-13 20:55:10 +0800

    add: prometheus to read response time

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
+
+Route::get('/metrics', function (CollectorRegistry $registry) {
+    $renderer = new RenderTextFormat();
+    return response($renderer->render($registry->getMetricFamilySamples()))
+        ->header('Content-Type', RenderTextFormat::MIME_TYPE);
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    $delay = random_int(1, 5);  // Random delay between 1 and 5 seconds
+    sleep($delay);
+    return 'Hello world!';
 });
